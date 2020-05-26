e56bfcc0b252803d804979a78c7f07bef8ebb5f7
Raweeroj Jeerapong <je.raweeroj_st@tni.ac.th>
2020-05-26 17:20:08 +0700

    Create component tasklist and create todo page

---
+++ src/pages/ToDoPage.vue
@@ -0,0 +1,17 @@
+<template>
+            <TaskList/>
+</template>
+
+<script>
+import TaskList from "../components/TaskList.vue"
+export default {
+    name: "ToDoPage",
+    components:{
+        TaskList
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
