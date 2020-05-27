b80075f9e771772989721ddedcb42aa6fa61d33a
Raweeroj Jeerapong <je.raweeroj_st@tni.ac.th>
2020-05-27 15:01:57 +0700

    Added Props

---
+++ src/pages/ArchivePage.vue
@@ -0,0 +1,35 @@
+<template>
+    <card-list :items="archives"></card-list>
+</template>
+<script>
+import CardList from '../components/CardList.vue'
+export default {
+    name:'ArchivePage',
+    components:{
+        CardList
+    },
+    data(){
+        return{
+            archives:[
+                {
+                    date:"25-05-2020",
+                    time:"17:00",
+                    tasks:[
+                        {id: 1,title : "Item A" ,status:"done"},
+                        {id: 2,title : "Item B" ,status:"done"}
+                    ]
+                },
+                {
+                    date:"26-05-2020",
+                    time:"17:00",
+                    tasks:[
+                        {id: 3,title : "Item C" ,status:"done"},
+                        {id: 4,title : "Item D" ,status:"done"}
+                    ]
+                }
+            ]
+        }
+    }
+}
+</script>
+
