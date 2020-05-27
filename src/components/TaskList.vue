ade9776f85767d8e3c7e989c4e706acf7909cdbc
Raweeroj Jeerapong <je.raweeroj_st@tni.ac.th>
2020-05-27 10:10:58 +0700

    Add Event Drag and Drop

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="container">
         <div class="task-zone">
-            <div class="drop-zone">
+            <div class="drop-zone" @drop="onDrop($event, 'todo')" @dragenter.prevent @dragover.prevent>
                 <h1>To-Do</h1>
-                <div class="drag-el" v-for="task in todoList" :key=task.id>{{task.title}}</div>
+                <div class="drag-el" draggable @dragstart="onStart($event, task)" v-for="task in todoList" :key=task.id>{{task.title}}</div>
             </div>
-             <div class="drop-zone">
+             <div class="drop-zone" @drop="onDrop($event, 'doing')" @dragenter.prevent @dragover.prevent>
                 <h1>Doing</h1>
-                <div class="drag-el" v-for="task in doingList" :key=task.id>{{task.title}}</div>
+                <div class="drag-el" draggable @dragstart="onStart($event, task)" v-for="task in doingList" :key=task.id>{{task.title}}</div>
             </div>
-             <div class="drop-zone">
+             <div class="drop-zone" @drop="onDrop($event, 'done')" @dragenter.prevent @dragover.prevent>
                 <h1>Done</h1>
-                <div class="drag-el" v-for="task in doneList" :key=task.id>{{task.title}}</div>
+                <div class="drag-el" draggable @dragstart="onStart($event, task)" v-for="task in doneList" :key=task.id>{{task.title}}</div>
             </div>
         </div>
     </div>
@@ -55,6 +55,18 @@ export default {
         },
         doneList(){
             return this.tasks.filter(task => task.status =="done")
+        }
+    },
+    methods:{
+        onStart(e, task){
+            e.dataTransfer.dropEffect = "move"
+            e.dataTransfer.effectAllow = "move"
+            e.dataTransfer.setData('taskId',task.id)
+        },
+        onDrop(e,newStatus){
+            const taskId = e.dataTransfer.getData('taskId')
+            const task = this.tasks.find(task => task.id == taskId)
+            task.status = newStatus
         }
     }
 }
